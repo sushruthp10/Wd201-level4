36adc8d4f1a3147e8460f112e14766da366263ab
sushruthp10 <113205970+sushruthp10@users.noreply.github.com>
2022-11-17 15:32:41 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-undef */
 const todoList = () => {
   all = [];
   const add = (todoItem) => {
@@ -9,37 +8,30 @@ const todoList = () => {
   };
 
   let today = new Date().toLocaleDateString("en-CA");
-  // let today = new Date().toISOString().split("T")[0];
-
   const overdue = () => {
     return all.filter((todo) => {
       return todo.dueDate < today;
     });
   };
-
   const dueToday = () => {
     return all.filter((todo) => {
       return todo.dueDate === today;
     });
   };
-
   const dueLater = () => {
     return all.filter((todo) => {
       return todo.dueDate > today;
     });
   };
-
   const toDisplayableList = (list) => {
     return list
       .map((todo) => {
         display_status = todo.completed ? "[x]" : "[ ]";
         display_date = todo.dueDate == today ? "" : todo.dueDate;
-
         return `${display_status} ${todo.title} ${display_date}`;
       })
       .join("\n");
   };
-
   return {
     all,
     add,
